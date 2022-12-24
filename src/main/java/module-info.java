c8ef2b3c808b04e23d841c2e8a234535b1eace11
fe-i <feic47@gmail.com>
2022-12-24 11:52:51 -0500

    workspace edits

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
-module com.example.demo {
+module com.example.treasurehunter {
     requires javafx.controls;
     requires javafx.fxml;
-
 
     opens com.example.treasurehunter to javafx.fxml;
     exports com.example.treasurehunter;
